1bd61ac43e4524b498d05bf883467f4b0023202a
qc_0dcdcfc4ed0b4f7f89500bb54c3818c2 <qc_0dcdcfc4ed0b4f7f89500bb54c3818c2@git.code.tencent.com>
2019-07-25 15:22:59 +0800

    明明很快了，为什么还是慢？

--- 10.py
+++ 10.py
@@ -1,5 +1,7 @@
 import time
 import profile
+
+
 class Solution:
     def isMatch_2(self, s: str, p: str) -> bool:
         i = 0
@@ -106,12 +108,13 @@ class Solution:
         return isMathFlag
 
     # 重新理逻辑，让*带着字符循环匹配，如果出现不匹配，则直接退出，或者匹配满对应s中的字母数量
-    def isMatch(self, s: str, p: str,k=0) -> bool:
+    def isMatch(self, s: str, p: str, k=0) -> bool:
+        # k代表进行到p的第几个字符了
 
-        length_s=len(s)
+        length_s = len(s)
 
         # 不用每次都来进行对p的操作
-        if(k==0):
+        if (k == 0):
             # 连续多个星号的情况完善
             i = 0
             while (True):
@@ -122,78 +125,90 @@ class Solution:
                 i += 1
 
             #  效率优化，先去掉重复的
-            tmp_list_i=[]
-            i=0
-            while(True):
-                if(i>=len(p)):break
-                if(i+3<len(p)):
-                    if(p[i] !='*'):
-                        if( p[i+1]=='*'):
-                            if( p[i+2]==p[i]):
-                                if( p[i+3]=='*'):
+            tmp_list_i = []
+            i = 0
+            while (True):
+                if (i >= len(p)): break
+                if (i + 3 < len(p)):
+                    if (p[i] != '*'):
+                        if (p[i + 1] == '*'):
+                            if (p[i + 2] == p[i]):
+                                if (p[i + 3] == '*'):
                                     tmp_list_i.append(i)
-                                    i+=1
-                i+=1
+                                    i += 1
+                i += 1
             for i in range(len(tmp_list_i)):
-                j=len(tmp_list_i)-1-i
-                m=tmp_list_i[j]
-                p=p[:m]+p[m+2:]
+                j = len(tmp_list_i) - 1 - i
+                m = tmp_list_i[j]
+                p = p[:m] + p[m + 2:]
 
-        def calStrCharRepeatNum(cal_str,cal_index):
-            cal_num=1
-            for i in range(cal_index,len(cal_str)-1):
-                if(cal_str[i]==cal_str[i+1]):
-                    cal_num+=1
+        def calStrCharRepeatNum(cal_str, cal_index):
+            cal_num = 1
+            for i in range(cal_index, len(cal_str) - 1):
+                if (cal_str[i] == cal_str[i + 1]):
+                    cal_num += 1
                 else:
                     break
             return cal_num
 
+        for i in range(k, len(p)):
 
-        for i in range(k,len(p)):
             if (p[i] == '*'):
                 if (i == 0):
+                    p = p[1:]
                     break
                 else:
                     charBeforStar = p[i - 1]
 
                     # 经过试验  发现不能对s进行处理，那么就需要转换一下思路，用s中对应字符重复的次数来作为p中*叠加次数的上限
                     # 如果*符号前是.符号，则长度最长可以是s余下的所有字符长度
-                    if (p[i - 1] == '.'):
-                        s_char_repeat_num = length_s - i + 1
-                    else:
-                        # 注意，这里的i-1的意思，是指跟p一样的字母的索引，而不是i（此时i对应的是*符号）
-                        s_char_repeat_num=calStrCharRepeatNum(s,i-1)
 
-                    #注意，这里的s_char_repeat_num+1是因为前闭后开
-                    for times in range(0, s_char_repeat_num+1):
-                        tmp_p = p[:i - 1]
-                        for in_times in range(times):
-                            tmp_p = tmp_p+charBeforStar
-                        tmp_p = tmp_p+p[i+1:]
-                        isMatchFlag_times = self.isMatch(s, tmp_p,i+times)
-                        if (isMatchFlag_times == True):
-                            return True
+                    # if (charBeforStar == '.'):
+                    #     s_char_repeat_num = length_s - i + 1
+                    # else:
+                    #     # 注意，这里的i-1的意思，是指跟p一样的字母的索引，而不是i（此时i对应的是*符号）
+                    #     s_char_repeat_num=calStrCharRepeatNum(s,i-1)
+                    # 注意，上面在处理重复字符数量时，如果有.且同时有其他*符合的字母时，计算数量有问题，所以干脆全部都取剩余的全部字符数量算了
+                    s_char_repeat_num = length_s - i + 1
 
+                    # 注意，这里的s_char_repeat_num+1是因为前闭后开
+                    for times in range(0, s_char_repeat_num + 1):
+                        try:
+                            tmp_p = p[:i - 1]
+                            for in_times in range(times):
+                                tmp_p = tmp_p + charBeforStar
+                            tmp_p = tmp_p + p[i + 1:]
+                            isMatchFlag_times = self.isMatch(s, tmp_p, i + times)
+                            if (isMatchFlag_times == True):
+                                return True
+                        except:
+                            #  没有匹配上 注意，0次和1次是不同的
+                            if (times == 0):
+                                continue
+                            else:
+                                return False
 
-        if(length_s!=len(p)):
+        if (length_s != len(p)):
             return False
         for index_i in range(length_s):
-            if(p[index_i]=='.' or s[index_i]==p[index_i]  ):
+            if (p[index_i] == '.' or s[index_i] == p[index_i]):
                 continue
             else:
+                if (index_i < k):
+                    raise Exception()
                 return False
         return True
 
     # 重大调整，将n次转换成0次和1次，即同时先对s和p进行简化  经测试  是失败的  错误的
-    def isMatch_4(self, s: str, p: str,k=0) -> bool:
+    def isMatch_4(self, s: str, p: str, k=0) -> bool:
         # p中没有星号，则直接匹配
-        hasStar=False
+        hasStar = False
         for i in range(len(p)):
-            if(p[i]=='*' and i>=1):
-                hasStar=True
+            if (p[i] == '*' and i >= 1):
+                hasStar = True
                 break
 
-        if(hasStar==False):
+        if (hasStar == False):
             if (s == p):
                 return True
             if (len(s) != len(p)):
@@ -204,7 +219,6 @@ class Solution:
                         return False
                 return True
 
-
         # p连续多个星号的情况完善
         i = 0
         while (True):
@@ -212,46 +226,46 @@ class Solution:
             if (i + 1 < len(p)):
                 if (p[i] == p[i + 1] == '*'):
                     p = p[:i] + p[i + 1:]
-                    i=-1
+                    i = -1
             i += 1
 
         #  p效率优化，先去掉重复的
-        tmp_list_i=[]
-        i=0
-        while(True):
-            if(i>=len(p)):break
-            if(i+3<len(p)):
-                if(p[i] !='*' and p[i+1]=='*' and p[i+2]==p[i] and p[i+3]=='*'):
+        tmp_list_i = []
+        i = 0
+        while (True):
+            if (i >= len(p)): break
+            if (i + 3 < len(p)):
+                if (p[i] != '*' and p[i + 1] == '*' and p[i + 2] == p[i] and p[i + 3] == '*'):
                     tmp_list_i.append(i)
-                    i+=1
-            i+=1
+                    i += 1
+            i += 1
         for i in range(len(tmp_list_i)):
-            j=len(tmp_list_i)-1-i
-            m=tmp_list_i[j]
-            p=p[:m]+p[m+2:]
+            j = len(tmp_list_i) - 1 - i
+            m = tmp_list_i[j]
+            p = p[:m] + p[m + 2:]
 
         # p的*符号前后字母一样时，直接将后一个字母去掉
         for i in range(len(p)):
-            if(i+2<len(p)):
-                if(p[i]==p[i+2] and p[i+1]=='*'):
-                    if(i+3<len(p)):
-                        p=p[:i+2]+p[i+3]
+            if (i + 2 < len(p)):
+                if (p[i] == p[i + 2] and p[i + 1] == '*'):
+                    if (i + 3 < len(p)):
+                        p = p[:i + 2] + p[i + 3]
                     else:
-                        p=p[:i+2]
+                        p = p[:i + 2]
 
         # todo  不能这样处理，因为这样会有问题，相当于不确定了连续字母的个数，而如果p中是确定的个数时，则会出现匹配不上的情况
 
         # s的连续字母简化
-        i=0
-        tmp_list_s=list(s)
-        while(True):
-            if(i>len(s)):
+        i = 0
+        tmp_list_s = list(s)
+        while (True):
+            if (i > len(s)):
                 break
-            if(i+1<len(s)):
-                if(s[i]==s[i+1]):
-                    tmp_list_s[i+1]='*'
-            i+=1
-        s=''.join(tmp_list_s)
+            if (i + 1 < len(s)):
+                if (s[i] == s[i + 1]):
+                    tmp_list_s[i + 1] = '*'
+            i += 1
+        s = ''.join(tmp_list_s)
         # s连续多个星号的情况完善
         i = 0
         while (True):
@@ -259,25 +273,25 @@ class Solution:
             if (i + 1 < len(s)):
                 if (s[i] == s[i + 1] == '*'):
                     s = s[:i] + s[i + 1:]
-                    i=-1
+                    i = -1
             i += 1
 
-        #匹配逻辑
-        for i in range(k,len(p)):
+        # 匹配逻辑
+        for i in range(k, len(p)):
             if (p[i] == '*'):
                 if (i == 0):
                     break
                 else:
-                    if(i-2==0):
-                        tmp_p = p[0] + p[i+1:]
+                    if (i - 2 == 0):
+                        tmp_p = p[0] + p[i + 1:]
                     else:
-                        tmp_p = p[:i - 1] + p[i+1:]
+                        tmp_p = p[:i - 1] + p[i + 1:]
                     # *号匹配0次
-                    isMatchFlag = self.isMatch(s, tmp_p,i)
+                    isMatchFlag = self.isMatch(s, tmp_p, i)
                     if (isMatchFlag == False):
                         tmp_p = p
                         # *号匹配1次
-                        isMatchFlag_times = self.isMatch(s, tmp_p,i+1)
+                        isMatchFlag_times = self.isMatch(s, tmp_p, i + 1)
                         if (isMatchFlag_times == True):
                             return True
                     else:
@@ -289,7 +303,7 @@ class Solution:
             return False
         else:
             for inner_i in range(len(s)):
-                if(inner_i-1>=0 and p[inner_i-1]=='.' and p[inner_i]=='*'):
+                if (inner_i - 1 >= 0 and p[inner_i - 1] == '.' and p[inner_i] == '*'):
                     continue
                 if (inner_i - 1 >= 0 and s[inner_i - 1] == p[inner_i] and s[inner_i] == '*'):
                     continue
@@ -297,10 +311,11 @@ class Solution:
                     return False
         return True
 
+
 if __name__ == '__main__':
     so = Solution()
-    s = 'bcbacacbacbbbbcac'
-    p = '..*a*a*b*c*.*a*bb*.'
+    s = 'babbcabcaabbbacaca'
+    p = 'bb*.c*.c*b*b.*c'
     start = time.perf_counter()
 
     res = so.isMatch(s, p)
